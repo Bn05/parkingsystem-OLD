06864a76e8bb7ad43fbd92da2f43bed9f8583bc8
Bertrand <bertrandn5@gmail.com>
2022-10-17 10:21:51 +0200

    add STORY_2 : -5% 5%-discount for recurring users

---
+++ src/test/java/com/parkit/parkingsystem/integration/dao/TicketDAOTest.java
@@ -0,0 +1,11 @@
+package com.parkit.parkingsystem.integration.dao;
+
+import com.parkit.parkingsystem.dao.TicketDAO;
+
+public class TicketDAOTest {
+
+    TicketDAO ticket = new TicketDAO();
+
+
+
+}
